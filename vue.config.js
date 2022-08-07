8bef61ae60d2d7d1845b341558f2c742d0422718
shunya9811 <11wash27@gmail.com>
2022-08-07 16:25:27 +0900

    relese

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/weather-app/'   //任意
-    : '/'
-}
+  transpileDependencies: ["vuetify"],
+  outputDir: "docs",
+  assetsDir: "./",
+  publicPath: "./",
+};
